7f985ebc7a051839d48da185f02f67d0cb9b8d46
ProPairApp <propair.app@gmail.com>
2025-09-06 16:06:33 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true };
-module.exports = nextConfig;
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: {
-    // TEMP: let the app build even if TypeScript finds errors.
-    // We'll turn this back off after we remove the old file.
-    ignoreBuildErrors: true,
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
